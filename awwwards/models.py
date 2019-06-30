4d7f20f096cfd8546fa34a60012daf72ad2ccfe7
Santa Claus <santa@northpole.com>
2019-06-30 18:56:32 +0300

    created class  profile

--- awwwards/models.py
+++ awwwards/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'images/',blank=True)
+    bio = models.TextField(max_length = 50,null = True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.comment
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def search_profile(cls, search_term):
+        userprofile = Profile.objects.filter(user__icontains = search_term)
+        return userprofile
+
