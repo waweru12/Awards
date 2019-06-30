6f2853ff6f58b2e234bccfb890ae95c576c26710
Santa Claus <santa@northpole.com>
2019-06-30 18:53:42 +0300

    styled submit button on login.html

--- awwwards/admin.py
+++ awwwards/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Project,Profile,UsabilityRating,ContentRating,DesignRating
+
 
 # Register your models here.
+
+admin.site.register(Project)
+admin.site.register(ContentRating)
+admin.site.register(UsabilityRating)
+admin.site.register(DesignRating)
+admin.site.register(Profile)
+
