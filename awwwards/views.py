274bc280858a6226169eef405195666f28820d03
Santa Claus <santa@northpole.com>
2019-06-30 18:59:49 +0300

    defined upload

--- awwwards/views.py
+++ awwwards/views.py
@@ -12,6 +12,26 @@ def home(request):
     return render(request, 'index.html', {"projects": projects})
 
 
+@login_required(login_url='/accounts/login/')
+def upload(request):
+    current_user = request.user
+    # try:
+    #     current_profile = Profile.objects.get(user_id=id)
+    # except ObjectDoesNotExist:
+    #     return redirect(update_profile, current_user.id)
+
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = current_user
+            project.save()
+        return redirect(home)
+
+    else:
+        form = NewProjectForm()
+    return render(request, 'new-project.html', {"form": form, "user": current_user})
+
 
 @login_required(login_url='/accounts/login/')
 def profile(request,id):
