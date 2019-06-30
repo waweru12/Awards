b9f0905ac1d43964fed2f9c45040a1ae3730072b
Santa Claus <santa@northpole.com>
2019-06-30 19:01:25 +0300

    added upload url to urls.py

---
+++ awwwards/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns=[
+    url(r'^$',views.home,name='homePage'),
+    url(r'^upload$',views.upload,name='upload'),
+    url(r'^ratecontent/(\d+)',views.add_content, name='ratecontent'),
+    url(r'^ratedesign/(\d+)',views.add_design, name='ratedesign'),
+    url(r'^rateusability/(\d+)',views.add_usability, name='rateusability'),
+    
+
+
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
