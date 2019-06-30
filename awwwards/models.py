e551ade4b27ed159818541113f2133304c0204d9
Santa Claus <santa@northpole.com>
2019-06-30 18:57:31 +0300

    created class project

--- awwwards/models.py
+++ awwwards/models.py
@@ -19,3 +19,77 @@ class Profile(models.Model):
         userprofile = Profile.objects.filter(user__icontains = search_term)
         return userprofile
 
+class Project(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    project_name = models.CharField(max_length =10)
+    project_url = models.CharField(max_length =50)
+    user= models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def save_project(self):
+        self.save()
+    
+   
+    @classmethod
+    def get_all_projects(cls):
+        project = cls.objects.all()
+        return project
+
+    @classmethod
+    def find_project_id(cls, id):
+        project_result = cls.objects.get(project_id=id)
+        return project_result
+# START OF THE CLASSES FOR RATING 
+
+class DesignRating(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6,'6'),
+        (7,'7'),
+        (8,'8'),
+        (9,'9'),
+        (10,'10')
+    )
+    project = models.ForeignKey('Project')
+    user = models.ForeignKey(User)
+    rating = models.IntegerField(choices=RATING_CHOICES, null=True)
+    
+class UsabilityRating(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6,'6'),
+        (7,'7'),
+        (8,'8'),
+        (9,'9'),
+        (10,'10')
+    )
+    project = models.ForeignKey('Project')
+    user = models.ForeignKey(User)
+    rating = models.IntegerField(choices=RATING_CHOICES, null=True)
+    
+
+class ContentRating(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6,'6'),
+        (7,'7'),
+        (8,'8'),
+        (9,'9'),
+        (10,'10')
+    )
+    project = models.ForeignKey('Project')
+    user = models.ForeignKey(User)
+    rating = models.IntegerField(choices=RATING_CHOICES, null=True)
+    
+
