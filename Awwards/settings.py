3232586da0eed2b21a54e8180c5406596dce5e15
Santa Claus <santa@northpole.com>
2019-07-01 08:01:20 +0300

    added urls api

--- Awwards/settings.py
+++ Awwards/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'bootstrap4',
     'awwwards'
 
@@ -72,6 +73,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Awwards.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
--- awwwards/views.py
+++ awwwards/views.py
@@ -114,3 +114,95 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request,'search.html',{"message":message})
+
+
+class ProjectList(APIView):
+   def get(self, request, format=None):
+       all_project = Project.objects.all()
+       serializers = ProfileSerializer(all_profile, many=True)
+       return Response(serializers.data)
+
+   def post(self, request, format=None):
+       serializers = ProjectSerializer(data=request.data)
+       if serializers.is_valid():
+           serializers.save()
+           return Response(serializers.data, status=status.HTTP_201_CREATED)
+       return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+   permission_classes = (IsAdminOrReadOnly,)
+
+
+class ProjectDescription(APIView):
+   permission_classes = (IsAdminOrReadOnly,)
+
+   def get_project(self, pk):
+       try:
+           return Project.objects.get(pk=pk)
+       except Project.DoesNotExist:
+           return Http404
+
+   def get(self, request, pk, format=None):
+       project = self.get_project(pk)
+       serializers = ProjectSerializer(project)
+       return Response(serializers.data)
+
+   def put(self, request, pk, format=None):
+       project = self.get_project(pk)
+       serializers = ProjectSerializer(project, request.data)
+       if serializers.is_valid():
+           serializers.save()
+           return Response(serializers.data)
+
+       else:
+           return Response(serializers.errors,
+                           status=status.HTTP_400_BAD_REQUEST)
+
+   def delete(self, request, pk, format=None):
+       project = self.get_project(pk)
+       project.delete()
+       return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Profile serializer
+class ProfileList(APIView):
+   def get(self, request, format=None):
+       all_profile = Profile.objects.all()
+       serializers = ProfileSerializer(all_profile, many=True)
+       return Response(serializers.data)
+
+   def post(self, request, format=None):
+       serializers = ProfileSerializer(data=request.data)
+       if serializers.is_valid():
+           serializers.save()
+           return Response(serializers.data, status=status.HTTP_201_CREATED)
+       return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+   permission_classes = (IsAdminOrReadOnly,)
+
+
+class ProfileDescription(APIView):
+   permission_classes = (IsAdminOrReadOnly,)
+   def get_profile(self, pk):
+       try:
+           return Profile.objects.get(pk=pk)
+       except Profile.DoesNotExist:
+           return Http404
+
+   def get(self, request, pk, format=None):
+       profile = self.get_profile(pk)
+       serializers = ProfileSerializer(profile)
+       return Response(serializers.data)
+
+
+   def put(self, request, pk, format = None):
+       profile = self.get_profile(pk)
+       serializers = ProfileSerializer(profile, request.data)
+       if serializers.is_valid():
+           serializers.save()
+           return Response(serializers.data)
+
+       else:
+           return Response(serializers.errors,
+                           status = status.HTTP_400_BAD_REQUEST)
+
+   def delete(self, request, pk, format=None):
+       profile = self.get_profile(pk)
+       profile.delete()
+       return Response(status=status.HTTP_204_NO_CONTENT)
