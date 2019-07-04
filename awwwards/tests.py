ed293d938a20a9b9188da43b7fe09702fe13b487
Santa Claus <santa@northpole.com>
2019-07-04 11:18:14 +0300

    updared tests

--- awwwards/tests.py
+++ awwwards/tests.py
@@ -1,45 +1,39 @@
 from django.test import TestCase
-from .models import Project,Profile
-from django.core.files.uploadedfile import SimpleUploadedFile
+from django.contrib.auth.models import User
+from .models import Profile, Project, Rating
 
-
-class ProjectTestCLass(TestCase):
-    '''
-    setup self instance of project
-    '''
+# Create your tests here.
+class ProjectTestCase(TestCase):
     def setUp(self):
-        self.post = Project(project_name='Awwards',project_url='www.awards.com')
-    
-    ''' 
-    test instance of project
-    '''
+        self.user = User.objects.create(id=2,username='a')
+        self.newproject = Project(image='media/insta/Fashion.jpg',project_name='Fashion',project_description='Delicious',id =1,url='http://127.0.0.1:8000/',profile=self.user)
+        
     def test_instance(self):
-        self.assertTrue(isinstance(self.post,Project))
-
-
-    '''
-    test save project
-    '''
+        self.assertTrue(isinstance(self.newproject,Project))
 
     def test_save_image(self):
-        self.post.save_project()
+        self.newproject.save()
         project = Project.objects.all()
         self.assertTrue(len(project)>0)
 
-class profileTestCLass(TestCase):
-    '''
-    setup self instance of profile
-    '''
+class ProfileTest(TestCase):
     def setUp(self):
-        self.prof = Profile(Bio='Let it be')
-    
-    ''' 
-    test instance of profile
-    '''
-    def test_instance(self):
-        self.assertTrue(isinstance(self.prof,Profile))
+        self.user = User.objects.create(id =1,username='a')
+        self.new_profile = Profile(name=self.user, profile_pic='media/instagram/photo.jpg',bio='I am awesome')
 
-    def test_save_profile(self):
-        self.prof.save_profile()
-        bio = Profile.objects.all()
-self.assertTrue(len(bio)>0)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+
+    def test_save_image(self):
+        self.new_profile.save()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)>0)
+
+class RatingtTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id =1,username='a')
+        self.newproject = Project(image='media/awards/Fashion.jpg',project_name='Fashion',project_description='Delicious',id =1,url='http://127.0.0.1:8000/',profile=self.user)
+        self.new_rating = Comment(project=self.newproject,user=self.user,design='2',usability='5',content='9',comment='You are awesome',)
+   
+    def test_instance(self):
+self.assertTrue(isinstance(self.new_rating,Comment))
