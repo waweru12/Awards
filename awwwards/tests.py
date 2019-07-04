9b7fb573d1c30568f02a8a2e0b27f390b317002a
Santa Claus <santa@northpole.com>
2019-07-04 11:06:30 +0300

    updated tests

--- awwwards/tests.py
+++ awwwards/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from .models import ContentRating,Project,Profile
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-# Create your tests here.
+
+class ProjectTestCLass(TestCase):
+    '''
+    setup self instance of project
+    '''
+    def setUp(self):
+        self.post = Project(project_name='awward',project_url='www.awards.com')
+    
+    ''' 
+    test instance of project
+    '''
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post,Project))
+
+
+    '''
+    test save project
+    '''
+
+    def test_save_image(self):
+        self.post.save_project()
+        project = Project.objects.all()
+        self.assertTrue(len(project)>0)
+
+class profileTestCLass(TestCase):
+    '''
+    setup self instance of profile
+    '''
+    def setUp(self):
+        self.prof = Profile(Bio='Live the moment')
+    
+    ''' 
+    test instance of profile
+    '''
+    def test_instance(self):
+        self.assertTrue(isinstance(self.prof,Profile))
+
+    def test_save_profile(self):
+        self.prof.save_profile()
+        bio = Profile.objects.all()
+self.assertTrue(len(bio)>0)
